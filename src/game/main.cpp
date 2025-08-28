d18124b66cf1c66bc3c11607c0c83bc48976c3a6
Henrik Jangefelt <Henke9105@hotmail.com>
2025-08-28 08:34:08 +0200

    Fixed start point of application

---
+++ src/game/main.cpp
@@ -0,0 +1,27 @@
+#include "game/game.h"
+#include <engine/core/engine.h>
+#include <iostream>
+
+
+// Temp?
+//#include <engine/subsystems/subsystem_registry.h>
+
+// TODO: fix precompiled header
+// TODO; use snake_case for classes instead? and functions?
+
+int main()
+{
+	Game game;
+	cursed_engine::Engine engine{ game };
+
+	if (!engine.init())
+	{
+		std::cerr << "Failed to initialize game!";
+		return -1;
+	}
+
+	engine.run();
+	engine.shutdown();
+
+	return 0;
+}
