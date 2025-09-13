43b93989c3457b775cdd72cf8178a49ce212484c
Henrik Jangefelt <Henke9105@hotmail.com>
2025-09-13 20:15:48 +0200

    Updated input handler

--- include/engine/input/input_handler.h
+++ include/engine/input/input_handler.h
@@ -1,24 +1,61 @@
 #pragma once
+#include "input_config.h"
 
 union SDL_Event;
 
 namespace cursed_engine
 {
-	struct InputConfig
+	enum class KeyState
 	{
-		// store key bindings..? (read from json)
+		Pressed,
+		Held,
+		Released,
+		None
+	};
+
+	struct KeyInfo
+	{
+		KeyState keyState = KeyState::None;
+
+		bool isDown = false;
+		bool wasDown = false;
+	};
+
+	struct MouseInfo
+	{
+		float x, y;
 	};
 
 	class InputHandler
 	{
 	public:
-		void processInput(SDL_Event* inputEvent);
+		void init(const InputConfig& config); // copy move instead?
+		void processInput(const SDL_Event& event);
+		void update();
 
-		[[nodiscard]] bool isKeyPressed() const; // pass in key!
-		[[nodiscard]] bool isKeyHeld() const;
+		// Bind action function (action, key)
+
+		[[nodiscard]] bool isKeyPressed(SDL_Scancode code) const;
+		[[nodiscard]] bool isKeyHeld(SDL_Scancode code) const;
+		[[nodiscard]] bool isKeyReleased(SDL_Scancode code) const;
+
+		[[nodiscard]] bool isMouseBtnPressed(uint8_t button) const;
+		[[nodiscard]] bool isMouseBtnHeld(uint8_t button) const;
+		[[nodiscard]] bool isMouseBtnReleased(uint8_t button) const;
+
+		[[nodiscard]] std::pair<float, float> getMousePosition() const;
+		[[nodiscard]] std::pair<float, float> getMouseDelta() const;
+		[[nodiscard]] float getMouseScroll() const;
 
 	private:
+		void processKeyEvent(const SDL_Event& event);
+		void processMouseEvent(const SDL_Event& event);
+
+		[[nodiscard]] KeyState getKeyState(const KeyInfo& info) const noexcept;
+
 		// registered key mappings? (config?)
-		std::unordered_map<int, bool> m_keyStates; // store key state?
+		//std::array<eKeyState, SDL_NUM_SCANCODES> m_keyStates; // store key state?
+		InputConfig m_config;
+		std::unordered_map<SDL_Scancode, KeyInfo> m_keyInfo; // scan code(s) or key codes?
 	};
 }
--- src/engine/input/input_handler.cpp
+++ src/engine/input/input_handler.cpp
@@ -1,24 +1,115 @@
 #include "engine/input/input_handler.h"
 #include <SDL3/SDL.h>
+#include <algorithm>
+#include <functional>
+#include <string>
+#include "engine/core/logger.h"
 
 namespace cursed_engine
 {
-	void InputHandler::processInput(SDL_Event* inputEvent)
+	void InputHandler::init(const InputConfig& config)
 	{
-		if (inputEvent->type == SDL_EVENT_KEY_DOWN)
+		m_config = config;
+
+		std::for_each(m_config.keyBindings.begin(), m_config.keyBindings.end(),
+			[&](const auto& pair) { m_keyInfo[pair.first] = KeyInfo{ KeyState::None, false, false }; });
+	}
+
+	void InputHandler::processInput(const SDL_Event& event)
+	{
+		switch (event.type)
 		{
-			if (inputEvent->key.key == SDLK_W)
-				std::cout << "UP";
+		case SDL_EVENT_KEY_UP:
+		case SDL_EVENT_KEY_DOWN:
+			processKeyEvent(event);
+			break;
+		case SDL_EVENT_MOUSE_MOTION:
+		case SDL_EVENT_MOUSE_BUTTON_DOWN:
+		case SDL_EVENT_MOUSE_BUTTON_UP:
+			processMouseEvent(event);
+			break;
+		default:
+			break;
 		}
 	}
 
-	bool InputHandler::isKeyPressed() const
+	void InputHandler::update()
+	{
+		for (auto& [scancode, info] : m_keyInfo)
+		{
+			info.keyState = getKeyState(info);
+			info.wasDown = info.isDown;
+		}
+	}
+
+	bool InputHandler::isKeyPressed(SDL_Scancode code) const
+	{
+		// TODO; will crash if key not mapped!
+		return m_keyInfo.at(code).keyState == KeyState::Pressed;
+	}
+
+	bool InputHandler::isKeyHeld(SDL_Scancode code) const
+	{
+		return m_keyInfo.at(code).keyState == KeyState::Held;
+	}
+
+	bool InputHandler::isKeyReleased(SDL_Scancode code) const
+	{
+		return m_keyInfo.at(code).keyState == KeyState::Released;
+	}
+
+	bool InputHandler::isMouseBtnPressed(uint8_t button) const
 	{
 		return false;
 	}
 
-	bool InputHandler::isKeyHeld() const
+	bool InputHandler::isMouseBtnHeld(uint8_t button) const
 	{
 		return false;
+	}
+
+	bool InputHandler::isMouseBtnReleased(uint8_t button) const
+	{
+		return false;
+	}
+
+	std::pair<float, float> InputHandler::getMousePosition() const
+	{
+		return std::pair<float, float>();
+	}
+
+	std::pair<float, float> InputHandler::getMouseDelta() const
+	{
+		return std::pair<float, float>();
+	}
+
+	float InputHandler::getMouseScroll() const
+	{
+		return 0.0f;
+	}
+
+	void InputHandler::processKeyEvent(const SDL_Event& event)
+	{
+		if (auto it = m_config.keyBindings.find(event.key.scancode); it != m_config.keyBindings.end())
+		{
+			bool isPressed = event.type == SDL_EVENT_KEY_DOWN;
+			m_keyInfo[it->first].isDown = isPressed;
+		}
+	}
+
+	void InputHandler::processMouseEvent(const SDL_Event& event)
+	{
+		float x, y;
+		SDL_GetMouseState(&x, &y);
+
+	}
+
+	KeyState InputHandler::getKeyState(const KeyInfo& info) const noexcept
+	{
+		if (info.isDown && !info.wasDown) return KeyState::Pressed;
+		if (info.isDown && info.wasDown) return KeyState::Held;
+		if (!info.isDown && info.wasDown) return KeyState::Released;
+
+		return KeyState::None;
 	}
 }
