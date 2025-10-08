6c9fbafeb16ce239e439c013f8d53b28c8cfe034
Henrik Jangefelt <Henke9105@hotmail.com>
2025-10-09 00:21:24 +0200

    added some includes in pch

--- include/engine/pch.h
+++ include/engine/pch.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <algorithm>
 #include <assert.h>
 #include <iostream>
 #include <unordered_map>
+
+// TODO; look up if this work - or if cmake_pch.hxx should be used instead
