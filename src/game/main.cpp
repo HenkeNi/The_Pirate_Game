d95ce86b4d3a3f88bf7c36f1faa64cff2082c19c
Henke <Henke9105@hotmail.com>
2025-10-20 22:22:35 +0200

    Some comments

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -6,14 +6,11 @@
 // physics...
 // Events
 // ECS
-// config... (either each subsystem stores its own, or a EngineConfig class)?
 
 // TODO: fix precompiled header
 // TODO; naming convention: use snake_case for classes instead? and functions? lowercase for struct members?
-// rapidjson
 // put scenes in a graph? should ui be its own scene? (easy to pop if not wanting it)
 // Render batched tiles?
-
 // Layer stack? 
 
 int main()
