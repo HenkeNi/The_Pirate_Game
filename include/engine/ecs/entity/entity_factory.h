31ebda18d4552bffd88bfad1e5f73e84ed132f91
Henke <Henke9105@hotmail.com>
2025-11-25 09:11:46 +0100

    EntityFactory now accepts an AssetManager

--- include/engine/ecs/entity/entity_factory.h
+++ include/engine/ecs/entity/entity_factory.h
@@ -1,20 +1,31 @@
 #pragma once
-#include "entity_handle.h"
+#include "entity_handle.h" // or forward declare?
 #include <optional>
 
 namespace cursed_engine
 {
 	//class EntityHandle;
 	class ECSRegistry;
+	class AssetManager;
 
-	class EntityFactory
+	class EntityFactory // TODO; accept asset mangaer instead!
 	{
 	public:
-		EntityFactory(ECSRegistry& registry);
+		EntityFactory(AssetManager* assetManager = nullptr);
 
-		std::optional<EntityHandle> createFromPrefab(std::string_view prefab);
+		void setEcsRegistry(ECSRegistry* ecsRegistry);
+
+		// contain functions like createEnemy, etc? contain logic to determine which type? sets other data? random strength, etc?
+		std::optional<EntityHandle> createFromPrefab(const std::string& prefabID);
+		//std::optional<EntityHandle> instantiate(ECSRegistry& ecsRegistry, std::string_view prefab);
+
 
 	private:
-		ECSRegistry& m_registry;
+		friend class Engine;
+
+		void initialize(AssetManager* assetManager);
+
+		ECSRegistry* m_ecsRegistry;
+		AssetManager* m_assetManager;
 	};
 }
--- src/engine/ecs/entity/entity_factory.cpp
+++ src/engine/ecs/entity/entity_factory.cpp
@@ -1,14 +1,34 @@
 #include "engine/ecs/entity/entity_factory.h"
+#include "engine/assets/asset_manager.h"
 
 namespace cursed_engine
 {
-	EntityFactory::EntityFactory(ECSRegistry& registry)
-		: m_registry{ registry }
+	/*EntityFactory::EntityFactory(ECSRegistry& ecsRegistry, PrefabRegistry& prefabRegistry)
+		: m_ecsRegistry{ ecsRegistry }, m_prefabRegistry{ prefabRegistry }
+	{
+	}*/
+
+	EntityFactory::EntityFactory(AssetManager* assetManager)
+		: m_ecsRegistry{ nullptr }, m_assetManager{ assetManager }
 	{
 	}
 
-	std::optional<EntityHandle> EntityFactory::createFromPrefab(std::string_view prefab)
+	void EntityFactory::setEcsRegistry(ECSRegistry* ecsRegistry)
 	{
+		m_ecsRegistry = ecsRegistry;
+	}
+
+	std::optional<EntityHandle> EntityFactory::createFromPrefab(const std::string& prefabID)
+	{
+		//const auto& prefab = m_prefabRegistry->get(prefabID);
+
+		// TODO; construct entity...
+
 		return std::nullopt;
+	}
+
+	void EntityFactory::initialize(AssetManager* assetManager)
+	{
+		m_assetManager = assetManager;
 	}
 }
