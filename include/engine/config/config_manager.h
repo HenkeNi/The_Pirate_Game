21e45226a22f5b4f962bff798dc4d41ea49e62f9
Henke <Henke9105@hotmail.com>
2026-04-27 18:14:55 +0200

    Moved config types to config manger file

--- include/engine/config/config_manager.h
+++ include/engine/config/config_manager.h
@@ -1,20 +1,100 @@
 #pragma once
 #include "engine/core/subsystem.h"
 #include "config_types.h"
+#include "engine/input/action.h"
 #include <filesystem>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <SDL3/SDL.h> // REMOVE!
 
 namespace cursed_engine
 {
 	namespace fs = std::filesystem;
 
+	struct AppInfo
+	{
+		std::string name = "";
+		std::string identifier = "";
+		std::string version = "";
+	};
+
+	struct WindowConfig
+	{
+		std::string iconPath = "";
+		std::string title = ""; // here?
+
+		// Dimensions
+		int width = 1280;
+		int height = 720;
+
+		// Display Settings
+		bool fullscreen = false;
+		bool vsync = true;
+		bool resizable = true;
+	};
+
+	// shouldnt know about actions??
+	// TODO; forward declare SDL_Scancode?
+
+	struct InputConfig
+	{
+		using KeyBindings = std::unordered_map<SDL_Scancode, Action>;
+
+		InputConfig() = default;
+
+		InputConfig(const KeyBindings& bindings, float sensitivity)
+			: keyBindings{ bindings }, mouseSensitivity{ sensitivity }
+		{
+		}
+
+		KeyBindings keyBindings;
+		float mouseSensitivity = 1.f;
+	};
+
+	struct AudioConfig
+	{
+		float masterVolume = 1.f;
+		float musicVolume = 0.8f;
+		float sfxVolume = 0.9f;
+	};
+
+	struct RenderConfig
+	{
+		float brightness = 1.f;
+		float contrast = 1.f;
+	};
+
+	struct ResourceConfig
+	{
+		std::vector<std::string> validTextureFormats;
+		std::vector<std::string> validAudioFormats;
+		std::vector<std::string> validFontFormats;
+		std::unordered_map<std::string, std::string> resourceIdToPath; // or create ResourceRegistry class?
+
+		uint32_t framesBeforeUnload = 4000;
+		float offloadDelaySeconds = 5.0f;
+		bool autoOffload = true;
+		bool lazyLoad = true;
+	};
+
+	// Config manager instead?
+	/*struct EngineConfig
+	{
+		AppInfo appInfo;
+		WindowConfig windowConfig;
+		InputConfig inputConfig;
+		AudioConfig audioConfig;
+		RenderConfig renderConfig;
+	};*/
+
 	// config loader?
-	// remake into a struct?
 
 	// Settings? EngineConfig? ConfigHandler? or POD struct EngineConfig and ConfigLoader helper class?
 	class ConfigManager : public Subsystem
 	{
 	public:
-		bool loadAllConfigs(const fs::path& path);
+		bool loadConfig(const fs::path& path); // or config loader?
 		
 		[[nodiscard]] inline const AppInfo& getAppInfo() const noexcept { return m_appInfo; }
 
@@ -26,8 +106,9 @@ namespace cursed_engine
 
 		bool saveWindowConfigToFile(const fs::path& path);
 
-
 	private:
+		// bool loadAssetPaths();
+		
 		WindowConfig m_windowConfig;
 		RenderConfig m_renderConfig;
 
--- include/engine/config/config_types.h
+++ include/engine/config/config_types.h
@@ -7,65 +7,70 @@
 
 namespace cursed_engine
 {
-	struct AppInfo
-	{
-		std::string name = "";
-		std::string identifier = "";
-		std::string version = "";
-	};
+	//struct AppInfo
+	//{
+	//	std::string name = "";
+	//	std::string identifier = "";
+	//	std::string version = "";
+	//};
 
-    struct WindowConfig
-    {
-		std::string iconPath = "";
-        int width = 1280;
-        int height = 720;
-        bool fullscreen = false;
-        bool vsync = true;
-    };
+ //   struct WindowConfig
+ //   {
+	//	std::string iconPath = "";
+ //       int width = 1280;
+ //       int height = 720;
+ //       bool fullscreen = false;
+ //       bool vsync = true;
+ //   };
 
-	// shouldnt know about actions??
-	// TODO; forward declare SDL_Scancode?
+	//// shouldnt know about actions??
+	//// TODO; forward declare SDL_Scancode?
 
-	struct InputConfig
-	{
-		using KeyBindings = std::unordered_map<SDL_Scancode, Action>;
+	//struct InputConfig
+	//{
+	//	using KeyBindings = std::unordered_map<SDL_Scancode, Action>;
 
-		InputConfig() = default;
+	//	InputConfig() = default;
 
-		InputConfig(const KeyBindings& bindings, float sensitivity)
-			: keyBindings{ bindings }, mouseSensitivity{ sensitivity }
-		{
-		}
+	//	InputConfig(const KeyBindings& bindings, float sensitivity)
+	//		: keyBindings{ bindings }, mouseSensitivity{ sensitivity }
+	//	{
+	//	}
 
-		KeyBindings keyBindings;
-		float mouseSensitivity = 1.f;
-	};
+	//	KeyBindings keyBindings;
+	//	float mouseSensitivity = 1.f;
+	//};
 
-	struct AudioConfig
-	{
-		float masterVolume = 1.f;
-		float musicVolume = 0.8f;
-		float sfxVolume = 0.9f;
-	};
+	//struct AudioConfig
+	//{
+	//	float masterVolume = 1.f;
+	//	float musicVolume = 0.8f;
+	//	float sfxVolume = 0.9f;
+	//};
 
-	struct RenderConfig
-	{
-		float brightness = 1.f;
-		float contrast = 1.f;
-	};
+	//struct RenderConfig
+	//{
+	//	float brightness = 1.f;
+	//	float contrast = 1.f;
+	//};
 
-	struct ResourceConfig
-	{
-		std::vector<std::string> validTextureFormats;
-	};
+	//struct ResourceConfig
+	//{
+	//	std::vector<std::string> validTextureFormats;
+	//	std::unordered_map<std::string, std::string> resourceIdToPath; // or create ResourceRegistry class?
 
-	// Config manager instead?
-	struct EngineConfig
-	{
-		AppInfo appInfo;
-		WindowConfig windowConfig;
-		InputConfig inputConfig;
-		AudioConfig audioConfig;
-		RenderConfig renderConfig;
-	};
+	//	bool lazyLoad = true;
+	//	bool autoOffload = true;
+	//	float offloadDelaySeconds = 5.0f;
+	//};
+
+	//// Config manager instead?
+	//struct EngineConfig
+	//{
+	//	AppInfo appInfo;
+	//	WindowConfig windowConfig;
+	//	InputConfig inputConfig;
+	//	AudioConfig audioConfig;
+	//	RenderConfig renderConfig;
+	//};
 }
