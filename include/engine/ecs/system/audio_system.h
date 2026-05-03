925eab43e4fee011bbf51d194ac21edbf2404402
Henke <Henke9105@hotmail.com>
2026-05-03 19:47:03 +0200

    Made audio system listen to audio event

--- include/engine/ecs/system/audio_system.h
+++ include/engine/ecs/system/audio_system.h
@@ -5,16 +5,18 @@
 namespace cursed_engine
 {
 	class AudioController;
+	class EventBus;
 
 	class AudioSystem : public System
 	{
 	public:
-		AudioSystem(AudioManager& audioManager, AudioController& audioController);
+		AudioSystem(AudioManager& audioManager, AudioController& audioController, EventBus& eventBus);
 
 		void update(SystemContext& context) override;
 
 	private:
 		AudioManager& m_audioManager;
 		AudioController& m_audioController;
+		EventBus& m_eventBus;
 	};
 }
--- src/engine/ecs/system/audio_system.cpp
+++ src/engine/ecs/system/audio_system.cpp
@@ -1,17 +1,38 @@
 #include "engine/ecs/system/audio_system.h"
 #include "engine/ecs/component/core_components.h"
 #include "engine/ecs/ecs_registry.h"
+#include "engine/events/event_bus.h"
+#include "engine/events/events.h"
 #include "engine/audio/audio_controller.h"
 
 namespace cursed_engine
 {
-	AudioSystem::AudioSystem(AudioManager& audioManager, AudioController& audioController)
-		: m_audioManager{ audioManager }, m_audioController{ audioController }
+	AudioSystem::AudioSystem(AudioManager& audioManager, AudioController& audioController, EventBus& eventBus)
+		: m_audioManager{ audioManager }, m_audioController{ audioController }, m_eventBus{ eventBus }
 	{
+		m_eventBus.subscribe<PlaySoundEvent>(
+			[&](PlaySoundEvent e) 
+			{
+				// or put in queue?
+				auto audioHandle = m_audioManager.getHandleById("623175__aphom000__button-click-selection");
+				if (const auto* audio = m_audioManager.get(audioHandle))
+				{
+					m_audioController.playSound(audio->m_stream, audio->m_buffer, audio->m_length);
+				}
+
+				int x = 20;
+			});
+		/*m_eventBus.subscribe<PlaySoundEvent>(
+			[](Entity e) 
+			{
+				int x = 20;
+			});*/
 	}
 
 	void AudioSystem::update(SystemContext& context)
 	{
+		// holds map of which sound id to sound?
+
 		auto audioHandle = m_audioManager.getHandleById("707884__dave4884__pirates-song");
 
 		if (audioHandle.isValid())
