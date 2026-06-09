bc6787eec39b2d35df68213e45182d1e3e5a3e7a
Henke <Henke9105@hotmail.com>
2026-06-09 18:30:09 +0200

    Some clean up / refactoring of the window class

--- include/engine/platform/window/window.h
+++ include/engine/platform/window/window.h
@@ -1,72 +1,72 @@
 #pragma once
+#include "engine/utils/non_copyable.h"
 #include "engine/math/vec2.hpp"
-#include <filesystem>
 
 typedef struct SDL_Window SDL_Window;
 union SDL_Event;
 
 namespace cursed_engine
 {
+	// TODO; Add a window builder?
+	// TODO; handle multiple vsync "modes"?
+
 	struct WindowConfig;
 	struct Surface;
-	class Settings;
-	class PlatformModule;
 
-	//SDL_SetWindowPosition();
-	//SDL_SetWindowAspectRatio();
-
-	// window builder?
-
-	class Window
+	class Window : public NonCopyable
 	{
 	public:
-		// ==================== Lifecycle ====================
 		Window();
 		~Window();
 
-		bool create(const char* title, const WindowConfig& config);	// or rename: applyConfig();
+		Window(Window&&) = delete;
+		Window& operator=(Window&&) = delete;
+
+		// ==================== Lifecycle ====================
+		bool create(const char* title, const WindowConfig& config);
 		void destroy();
 
 		// ==================== Event Handling ====================
-		void processEvent(const SDL_Event& event); // make private... // platformModule is friend class
+		void processEvent(const SDL_Event& event);
 		
 		// ==================== Queries ====================
 		[[nodiscard]] inline SDL_Window* getHandle() const noexcept { return m_window; } // rename? getInternal?
+
+		[[nodiscard]] inline float getAspectRatio() const { return (float)m_size.x / (float)m_size.y; }
 
 		[[nodiscard]] inline int getWidth() const noexcept { return m_size.x; }
 
 		[[nodiscard]] inline int getHeight() const noexcept { return m_size.y; }
 
-		[[nodiscard]] inline float getAspectRatio() const { return (float)m_size.x / (float)m_size.y; }
-
 		[[nodiscard]] inline bool isFullscreen() const noexcept { return m_isFullscreen; }
 
 		[[nodiscard]] inline bool shouldClose() const noexcept { return m_shouldClose; }
 
-	private:
-		friend class Settings;
-		friend class PlatformModule; // remove? only allow icon and title to be set during init?
-
 		// ==================== Operations ====================
 		inline void toggleFullscreen() { setFullscreen(!m_isFullscreen); } // remove?
 
+		void setAspectRatio(float width, float height);
+
+		void setAlwaysOnTop(bool enable);
+
 		void setFullscreen(bool enable);
+
+		void setPosition(float x, float y);
 
 		void setVSync(bool enable); // TEST!
 
 		void setTitle(const char* title);
 
-		void setIcon(Surface surface); // best way of passing it?
+		void setIcon(Surface surface);
 
+	private:
 		// ==================== Interal Helpers ====================
 		void handleResize(int width, int height);
 
 		SDL_Window* m_window;
 		IVec2 m_size;
+		bool m_isVSyncEnabled;
 		bool m_isFullscreen;
 		bool m_shouldClose;
-
-		bool m_isVSyncEnabled = false;
-		// store bool is vsync is enabled? store vsync value? store defautl vsync value?
 	};
 }
--- src/engine/platform/window/window.cpp
+++ src/engine/platform/window/window.cpp
@@ -1,15 +1,14 @@
 #include "engine/platform/window/window.h"
-//#include "engine/config/config_types.h" or use this?!
-#include "engine/core/config/config_manager.h"
-#include "engine/core/logger.h"
 #include "engine/resources/texture/surface.h"
-#include <SDL3/SDL_video.h>
+#include "engine/core/settings/engine_config.h"
+#include "engine/core/logger.h"
 #include <SDL3/SDL_events.h>
+#include <SDL3/SDL_video.h>
 
 namespace cursed_engine
 {
 	Window::Window()
-		: m_window{ nullptr }, m_size{ 0, 0 }, m_isFullscreen{ false }, m_shouldClose{ false }
+		: m_window{ nullptr }, m_size{ 0, 0 }, m_isVSyncEnabled{ false }, m_isFullscreen{ false }, m_shouldClose{ false }
 	{
 	}
 
@@ -45,6 +44,8 @@ namespace cursed_engine
 	{
 		SDL_DestroyWindow(m_window);
 		m_window = nullptr;
+
+		Logger::logInfo("Window destroyed");
 	}
 
 	void Window::processEvent(const SDL_Event& event)
@@ -66,37 +67,48 @@ namespace cursed_engine
 		}
 	}
 
+	void Window::setAspectRatio(float width, float height)
+	{
+		SDL_SetWindowAspectRatio(m_window, width, height);
+		Logger::logDebub(std::format("Changed aspect ratio: {} x {}", width, height));
+	}
+
+	void Window::setAlwaysOnTop(bool enable)
+	{
+		SDL_SetWindowAlwaysOnTop(m_window, enable);
+		Logger::logDebub(std::format("Window always on top changed to: {}", enable));
+	}
+
 	void Window::setFullscreen(bool enable)
 	{
 		m_isFullscreen = enable;
 		SDL_SetWindowFullscreen(m_window, enable);
+
+		Logger::logDebub(std::format("Fullscreen: {}", enable ? "enabled" : "disabled"));
+	}
+
+	void Window::setPosition(float x, float y)
+	{
+		SDL_SetWindowPosition(m_window, x, y);
+		Logger::logDebub(std::format("Window position: {} {}", x, y));
 	}
 
 	void Window::setVSync(bool enable)
 	{
-		int vsync = 0;
-		bool success = SDL_GetWindowSurfaceVSync(m_window, &vsync);
-
-		if (!success)
-		{
-			Logger::logError("Failed to read vsync for window!");
-			return;
-		}
-		
-		if (vsync == 1)
-			return;
-
-		SDL_SetWindowSurfaceVSync(m_window, 1);
+		SDL_SetWindowSurfaceVSync(m_window, enable ? 1 : 0);
+		Logger::logDebub(std::format("Vsync: {}", enable ? "enabled" : "disabled"));
 	}
 
 	void Window::setTitle(const char* title)
 	{
 		SDL_SetWindowTitle(m_window, title);
+		Logger::logDebub(std::format("Window title updated: {}", title));
 	}
 	
 	void Window::setIcon(Surface surface)
 	{
 		SDL_SetWindowIcon(m_window, surface.surface);
+		Logger::logDebub("New window icon set!");
 	}
 
 	void Window::handleResize(int width, int height)
